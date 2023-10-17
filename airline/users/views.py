b242149962cab4a7120e732a9ce7d1a9cb1a6275
johCodes <johcodes19@gmail.com>
2023-10-17 20:42:27 +0300

    created some views: index, login_view, and logout_view for my app. if the user is not authenticated, they are taken to the login page. I have used HttpResponseRedirect, and reverse concepts.

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+#if the request object is not authenticated....
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    
+def login_view(request):
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    pass
