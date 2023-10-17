0cb09301f1ebfa2b991c59f38285dfe98d32cdbc
johCodes <johcodes19@gmail.com>
2023-10-17 21:41:00 +0300

    added a new app and did the basic setup in settings and urls.py

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'flights',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('flights/', include('flight.urls')),
     path('users/',include('users.urls'))
 ]
