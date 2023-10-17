ae125c34ab3efca23f8585402fd30195665be835
johCodes <johcodes19@gmail.com>
2023-10-17 22:10:53 +0300

    i have created a class called Flight which will have all the properties

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    #this class called flight will have all the properties a flight should have
+    origin= models.CharField(max_length=64)
+    destination= models.CharField(max_length=64)
+    duration= models.IntegerField()
+    #from here I will create migrations to tell django to take into account the changes I have made in my models and put them in a database
+    #to do that, you go to the terminal and type python manage.py makemigrations
