9263fb91c9a815dba7514feb857c30f83824d8a4
johCodes <johcodes19@gmail.com>
2023-10-20 17:22:15 +0300

    added some comments to the file on how to modify the value of the Flight class using the python shell. I have also added a function which will give the string representation of the flight

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -9,3 +9,13 @@ class Flight(models.Model):
     #from here I will create migrations to tell django to take into account the changes I have made in my models and put them in a database
     #to do that, you go to the terminal and type python manage.py makemigrations
     #then python manage.py migrate
+    #then python manage.py shell to enter the shell
+    '''
+    in the shell, run - from flights.models import Flight...... where flights is the name of the app and the other Flight is the class
+    from there you can add values to the Flight like this......
+    f= Flight(origin='New York', destination='London' , duration=415)
+    f.save()
+    to return a string representation of the Flight object, we do this,,,,
+    '''
+    def __str__(self) :
+        return f"{self.id}: {self.origin} to {self.destination} in {self.duration} minutes"
