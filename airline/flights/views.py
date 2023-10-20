f90bed479978565c7cf56743c4c71f3e0dc7d397
johCodes <johcodes19@gmail.com>
2023-10-20 20:10:37 +0300

    I have added another function for a flight, thich will git the primary key of a flight and render the result to a html file called flight.html

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -6,3 +6,11 @@ def index(request):
     return render(request, 'flights/index.html',{
         'flights' : Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    #first get the flight whose primary key is flight_id
+    flight= Flight.objects.get(pk=flight_id)
+    #then pass in the flight to a render request for flight.html
+    return render(request, 'flights/flight.html',{
+        'flight': flight
+    })
