64490444c1254d543ae2978732ea089cec5c8d0f
johCodes <johcodes19@gmail.com>
2023-10-21 13:57:15 +0300

    added some configuration settings to the admin site

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Flight, Airport, passenger
 
 # Register your models here.
-admin.site.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display= ('id', 'origin', 'destination', 'duration')
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal= ('flights',)
+
+
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
-admin.site.register(passenger)
+admin.site.register(passenger, PassengerAdmin)
