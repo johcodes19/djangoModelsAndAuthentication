6dc5a0c15182f74de888a75f911c78eb3170d031
johCodes <johcodes19@gmail.com>
2023-10-21 16:16:02 +0300

    added information to login the user in if they are authenticated or return the form back to them if they are not authenticated.

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -7,8 +8,24 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('users:login'))
+    return render(request,'users/user.html' )
     
 def login_view(request):
+    #if the request method is POST, first get the username and password
+    if request.method == 'POST':
+        username= request.POST['username']
+        password= request.POST['password']
+        #to authenticate the user, 
+        user= authenticate(request, username=username, password=password)
+        #if the user is authenticated or not None,.... log them in and return them to index 
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('users:index'))
+        #if the user is not authenticated, return the login form with a message saying invalid credentials
+        else:
+            return render(request, 'users/login.html',{
+                'message': 'Invalid credentials'
+            })
     return render(request, 'users/login.html')
 
 def logout_view(request):
