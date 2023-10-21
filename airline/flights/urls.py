471f0625e75f4eb3b3f55b549822e7805451ac72
johCodes <johcodes19@gmail.com>
2023-10-21 12:01:33 +0300

    added a link to got back to all flight list

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name= 'flights'
+
 urlpatterns= [
     path('', views.index, name='index'),
     path('<int:flight_id>', views.flight, name='flight')
